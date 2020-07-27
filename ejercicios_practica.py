9bf2d986fc33450f23ecf0342630730b9b4203ae
Dario-Rosario <65090915+Dario-Rosario@users.noreply.github.com>
2020-07-26 22:30:58 -0300

    Update ejercicios_practica.py

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -28,7 +28,7 @@ def ej1():
     # Se desea graficar tres funciones en una misma figura
     # en tres gráficos (axes) distintos. Para el siguiente
     # intervalor de valores de x:
-    x = list(range(-10, 11, 1))
+  
 
     # Realizar tres gráficos que representen
     # y1 = x^2 (X al cuadrado)
@@ -55,13 +55,50 @@ def ej1():
     # Cada gráfico realizarlo con un color distinto
     # a su elección
 
+    x = list(range(-10, 11, 1))
+    x2 =np.array(x)
+
+    y1 = x2**2
+    y2 = x2**3
+    y3 = x2**4 
+
+    gs = gridspec.GridSpec(3, 1)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[1, 0])
+    ax3 = fig.add_subplot(gs[2, 0])
+
+    ax1.plot(x2, y1, color = "b", label = "y1=x**2")
+    ax1.set_facecolor("Whitesmoke")
+    ax1.set_title("Funcion x cuadrado")
+    ax1.set_ylabel("Y")
+    ax1.set_xlabel("X")
+    ax1.legend()
+
+    ax2.plot(x2, y2, color = "c", label = "y2=x**3")
+    ax2.set_facecolor("Whitesmoke")
+    ax2.set_title("Funcion x al cubo")
+    ax2.set_ylabel("Y")
+    ax2.set_xlabel("X")
+    ax2.legend()
+
+    ax3.plot(x2, y3, color = "m", label = "y1=x**4")
+    ax3.set_facecolor("Whitesmoke")
+    ax3.set_title("Funcion x a la cuarta")
+    ax3.set_ylabel("Y")
+    ax3.set_xlabel("X")
+    ax3.legend()
+
+    plt.show()
+
+
 
 def ej2():
     # Scatter Plot
     # Se desea graficar dos funciones en una misma figura
     # en dos gráficos (axes) distintos. Para el siguiente
     # intervalor de valores de x:
-    x = np.arange(0, 4*np.pi, 0.1)
+
 
     # Realizar dos gráficos que representen
     # y1 = sin(x)
@@ -83,14 +120,29 @@ def ej2():
     # Cada gráfico realizarlo con un mark distinto
     # a su elección.
 
+    x = np.arange(0, 4*np.pi, 0.1)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+
+    sample_size = 20
+    fig = plt.figure()
+    fig.suptitle("Grafico de sin y cos", fontsize=14)
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+    
+    ax1.plot(x, y1, color="b", label = "y=sin(x)")
+    ax1.set_facecolor("Whitesmoke")
+
+    ax2.plot(x, y2, color="b", label = "y=cos(x)")
+    ax2.set_facecolor("Whitesmoke")
+
+    plt.show()
+
 
 def ej3():
     # Bar Plot
     # Generar un gráfico de barras simple a partir
     # de la siguiente información:
-
-    lenguajes = ['Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp']
-    performance = [10, 8, 6, 4, 2, 1]
 
     # Realizar un gráfico de barras en donde se pueda ver el uso
     # de cada lenguaje, se debe utilizar los labels "lenguajes"
@@ -98,6 +150,19 @@ def ej3():
 
     # Se debe colocar título al gráfico.
     # Se debe cambiar la grilla y el fondo a su elección.
+
+    lenguajes = ['Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp']
+    performance = [10, 8, 6, 4, 2, 1]
+
+    fig = plt.figure()
+    fig.suptitle("Usos de Lenguajes de Programacion")
+    ax = fig.add_subplot()
+    ax.bar(lenguajes, performance, label = "lenguajes")
+    ax.set_facecolor("Whitesmoke")
+    ax.set_ylabel("Performance")
+    ax.set_xlabel("Lenguajes de Programacion")
+    
+    plt.show()
 
 
 def ej4():
@@ -116,6 +181,14 @@ def ej4():
     # Se desea mostrar en el gráfico los porcentajes de c/u
     # Se debe colocar un título al gráfico
 
+    fig = plt.figure()
+    fig.suptitle("Uso de lenguajes de programacion")
+    ax = fig.add_subplot()
+
+    ax.pie(uso_lenguajes.values(), labels = uso_lenguajes.keys(), autopct = "%1.1f%%")
+    ax.axis("equal")
+    plt.show()
+
 
 def ej5():
     # Uso de múltiples líneas en un mismo gráfico (axes)
@@ -124,6 +197,20 @@ def ej5():
     step = 0.1
     sample_size = 100
     signal = [{'X': i*step, 'Y': math.sin(i*step)} for i in range(sample_size)]
+
+    signal_x = [i for i in signal["X"]]
+    signal_y = ["Y" for i in signal]
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    ax.plot(signal_x, signal_y, color = "b")
+    ax.set_facecolor("Whitesmoke")
+    ax.set_title("Senoidal")
+
+    plt.show()
+
+
 
     # Se generó una lista de diccionarios con dos columnas "X" e "Y"
     # que corresponden a los valores de nuestra señal senoidal.
@@ -157,8 +244,8 @@ def ej5():
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    ej1()
-    # ej2()
-    # ej3()
-    # ej4()
-    # ej5()
+    #ej1()
+    #ej2()
+    #ej3()
+    #ej4()
+    ej5()
